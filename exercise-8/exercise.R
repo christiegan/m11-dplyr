96b05c9b34d9da277dc37e990959b7553541577d
christiegan <chrisleegan@gmail.com>
2017-04-19 21:15:14 -0700

    made changes

--- exercise-8/exercise.R
+++ exercise-8/exercise.R
@@ -1,28 +1,30 @@
 # Exercise 8: Pulitzer Prizes
 
 # Read in the data
-pulitzer <- read.csv("data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
+pulitzer <- read.csv("C;/Users/Christie Gan/Desktop/INFO201/m11-dplyr/data/pulitzer-circulation-data.csv", stringsAsFactors = FALSE)
 
 # Install and load the needed libraries
 # Be sure to comment out the install.packages function so it won't install it every time it runs
 # Remeber you only need to install a package once
-#install.packages(dplyr)
+install.packages("dplyr")
 library(dplyr)
 
 # View in the data set. Start to understand what the data columns contains
 # Be sure to comment out the function so it won't view everytime you run the code.
-
+View(pulitzer)
 
 # Use 'colnames' to print out the names of the columns
+names <- colnames(pulitzer)
 
 
 # Use 'str' to print what types of values are contained in each column
 # Did any value type surprise you? Why do you think they are that type?
+str(names)
 
 
 # Add a column in a dataframe called 'Pulitzer.Prize.Change` that contains the diffrence in changes
 # in Pulitzer Prize Winners from 2004 to 2013 and Pultizer Prize Winners from 1990 to 2003.
-
+Pulitzer.Prize.Change <- mutate
 
 # What publication gained the most pulitzer prizes from 2004-2014?
 # Be sure to use the pipe operator! 
